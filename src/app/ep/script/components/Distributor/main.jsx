da1292356db2df3cc7e19522f2a466eb0f48366d
Jayin Ton <tonjayin@gmail.com>
2015-07-10 15:05:45 +0800

    ep:经销商页面，缺少了联系人名称 close #17

--- src/app/ep/script/components/Distributor/main.jsx
+++ src/app/ep/script/components/Distributor/main.jsx
@@ -13,20 +13,14 @@ module.exports = React.createClass({
 		var createItem = function(item){
 			return (
 				<div>
-					<div>
-					  <div className="inline-block">{item.Name}</div>
-					  <div className="inline-block text-margin">
-					  	<span>{item.Phone}</span>
-					  </div>
-					</div>
-					<div>
-					  <div className="inline-block text-margin">
-					  	<span >{item.Address}</span>
-					  </div>
-					</div>
+					<div>经销商：<span>{item.Name}</span></div>
+					<div>联系人：<span>{item.Contact}</span></div>
+					<div>电 话：<span>{item.Phone}</span></div>
+					<div>地 址：<span >{item.Address}</span></div>
 					<div>
 					  <div className="inline-block" style={{fontSize: '0.85rem'}}>{item.Note}</div>
 					</div>
+					<div className="driver"></div>
 				</div>
 			);
 		};
