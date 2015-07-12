264d2c5761e0724235909da3d420bdb32f216436
Jayin Ton <tonjayin@gmail.com>
2015-07-12 21:22:47 +0800

    ep:三级列表开放留言功能 close #14

--- src/app/ep/script/components/MessageList/main.jsx
+++ src/app/ep/script/components/MessageList/main.jsx
@@ -4,8 +4,11 @@
 var React = require('react');
 var app = require('app');
 var $ = require('jquery');
+var utils = require('../../utils');
 
 module.exports = React.createClass({
+	preMessage: '',
+	lastPostTime: 0,
 	getInitialState: function(){
 		return {
 			result: this.props.result || [],
@@ -42,15 +45,75 @@ module.exports = React.createClass({
 		var page = parseInt(React.findDOMNode(this.refs.pageIndex).textContent)
 		this.getMessage(page+1)
 	},
+	handleClickLeftMessage: function(){
+		var $input = $('.component-MessageList input');
+		if (Date.now - this.lastPostTime <= 1000){
+			app.alerts.alert('留言太快了，请休息一会儿再来');
+			return;
+		}
+		if($input.val() == ''){
+			app.alerts.alert('留言不能为空');
+			return;
+		}
+		if ($input.val() == this.preMessage){
+			app.alerts.alert('留言不能与上条相同')
+			return;
+		}
+		if (utils.isAllNumber($input.val()) || utils.isAllLetter($input.val())){
+			app.alerts.alert('留言不能全为英文、数字');
+			return;
+		}
+
+		app.ajax.post({
+			url: "/Data/CategoryNote/{category3ID}".replace('{category3ID}', this.state.category.id),
+			data: {
+				note: $input.val(),
+				companyCode: this.state.category.companyCode
+			},
+			success: function(res){
+				if (res.result){
+					this.preMessage = $input.val();
+					$input.val('');
+					app.alerts.alert('留言成功')
+				}else{
+					app.alerts.alert(res.message)
+				}
+
+			}.bind(this),
+			error: function(){
+				app.alerts.alert('系统繁忙，请稍后再试')
+			}
+		});
+	},
+	handleRefresh: function(){
+		this.getMessage(1)
+	},
 	render: function(){
 
 		return (
 			<div className="component-MessageList">
 				<div >
-					{this.state.result.Records && this.state.result.Records.map(function(messageObj){
+					{this.state.result.Records && this.state.result.Records.map(function(messageObj, index){
+						var color = 'black';
+						var square = {
+							backgroundColor: 'black',
+							height: '10px',
+							width: '10px',
+							display: 'inline-block',
+							padding: '4px'
+						};
+						if (index % 2 == 0){
+							color = 'darkgrey';
+							square.backgroundColor = 'darkgrey';
+						}
+						if (messageObj.ReplyToID != '' && messageObj.ReplyToID != 0 ){
+							color = 'red';
+							square.backgroundColor = 'red';
+						}
 						return (
 							<div className="message-body">
-								{messageObj.Message}
+								<span style={square}></span>
+								<span style={{color: color,marginLeft: '4px'}}>{messageObj.Message}</span>
 							</div>
 						)
 					}.bind(this))}
@@ -62,6 +125,11 @@ module.exports = React.createClass({
 					    {this.props.result.Index}
 					</div>
 					<div onClick={this.handleClickNext} className="inline-block  btn">下一页</div>
+					<div className="inline-block" style={{marginTop: '4px',width: '100%'}}>
+					    <input type="text" placeholder="内容" style={{width: '50%'}} />
+					    <button onClick={this.handleClickLeftMessage} className="inline-block">留言</button>
+					    <button onClick={this.handleRefresh} className="inline-block">刷新</button>
+					</div>
 				</div>
 			</div>
 		);
