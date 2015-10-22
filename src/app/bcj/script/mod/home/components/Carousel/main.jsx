37547c9a6bfdaebf324298ec39b477046e738950
Jayin Ton <tonjayin@gmail.com>
2015-10-22 15:50:38 +0800

    bcj: 完成图片轮播

--- src/app/bcj/script/mod/home/components/Carousel/main.jsx
+++ src/app/bcj/script/mod/home/components/Carousel/main.jsx
@@ -7,34 +7,58 @@ var slick = require('slick');
 var Carousel = React.createClass({
 	getInitialState: function(){
 		return {
+			advertisements: this.props.advertisements || []
 		};
 	},
     componentDidMount: function() {
-        $('.your-class').slick({
-            arrows: false,
-            accessibility: false,
-            fade: true,
-            speed: 300,
-            cssEase: 'linear',
-            slidesToShow: 1,
-            // dots: true,
-            // adaptiveHeight: true,
-            centerMode: true,
-            centerPadding: '60px',
-            autoplay: true,
-            autoplaySpeed: 2000
-        })
+		this.fetchData();
     },
+	fetchData: function(){
+		app.ajax.get({
+			url: '/bcj/api/carousel.json',
+			success: function(res){
+				// console.log(res)
+				this.setState({
+					advertisements: res
+				})
+				$('.your-class').slick({
+					arrows: false,
+					accessibility: false,
+					fade: true,
+					speed: 700,
+					cssEase: 'linear',
+					slidesToShow: 1,
+					// dots: true,
+					// adaptiveHeight: true,
+					centerMode: true,
+					centerPadding: '60px',
+					autoplay: true,
+					autoplaySpeed: 2000
+				});
+			}.bind(this),
+			error: function(){
+				app.alerts.alert('网络繁忙，获取轮播广告失败');
+			}
+		})
+	},
+	handleImgClick: function(advertisement){
+		location.href = advertisement.link;
+	},
 	render: function(){
         var imgStyle = {
             height: '120px'
         }
 		return (
             <div className="your-class">
-    			<img style={imgStyle} src="https://img.alicdn.com/tps/TB13g8QKXXXXXa7XXXXXXXXXXXX-1280-512.jpg_790x420Q50.jpg"></img>
-    			<img style={imgStyle} src="https://img.alicdn.com/tps/TB1LydzKXXXXXcRXpXXXXXXXXXX-1280-512.jpg_790x420Q50.jpg"></img>
-    			<img style={imgStyle} src="https://img.alicdn.com/tps/i2/TB1i8XuKXXXXXbGXFXXKLG.WVXX-1280-512.jpg_790x420Q50.jpg"></img>
-    			<img style={imgStyle} src="https://img.alicdn.com/tps/TB1gq82KXXXXXbIXXXXXXXXXXXX-1280-512.jpg_790x420Q50.jpg"></img>
+				{this.state.advertisements.map(function(advertisement){
+					return (
+						<img style={imgStyle}
+							src={advertisement.img}
+							onClick={this.handleImgClick.bind(this, advertisement)}
+							key={advertisement.link}
+							alt={advertisement.title}></img>
+					);
+				}.bind(this))}
     		</div>
 		);
 	}
